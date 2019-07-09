0fff5b4985497e05be4aefa5924bd1296c79030d
Derek Lawless <derek.lawless@gmail.com>
2019-07-09 23:57:46 +0100

    Adding recursive binary search example.

--- src/searching/binary-search/__tests__/binary-search.test.ts
+++ src/searching/binary-search/__tests__/binary-search.test.ts
@@ -1,4 +1,4 @@
-import { binarySearch } from "../binary-search";
+import { binarySearch, binarySearchRecursive } from "../binary-search";
 
 describe("binarySearch()", () => {
 	const values = [...Array(100).keys()];
@@ -32,7 +32,41 @@ describe("binarySearch()", () => {
 
 	it("returns the expected result if there's no match", () => {
 		expect(binarySearch(values, 100)).toEqual({
+			iterations: 8,
+			position: -1,
+		});
+	});
+});
+
+describe("binarySearchRecursive()", () => {
+	const values = [...Array(100).keys()];
+
+	it("returns the expected results for the matched values", () => {
+		expect(binarySearchRecursive(values, 0)).toEqual({
+			iterations: 6,
+			position: 0,
+		});
+		expect(binarySearchRecursive(values, 24)).toEqual({
+			iterations: 2,
+			position: 24,
+		});
+		expect(binarySearchRecursive(values, 49)).toEqual({
+			iterations: 1,
+			position: 49,
+		});
+		expect(binarySearchRecursive(values, 74)).toEqual({
+			iterations: 2,
+			position: 74,
+		});
+		expect(binarySearchRecursive(values, 99)).toEqual({
 			iterations: 7,
+			position: 99,
+		});
+	});
+
+	it("returns the expected result if there's no match", () => {
+		expect(binarySearchRecursive(values, 100)).toEqual({
+			iterations: 8,
 			position: -1,
 		});
 	});
--- src/searching/binary-search/binary-search.ts
+++ src/searching/binary-search/binary-search.ts
@@ -6,24 +6,54 @@ export interface IBinarySearchResult {
 export function binarySearch<T>(values: T[] = [], target: T): IBinarySearchResult {
 	let low = 0;
 	let high = values.length - 1;
-	let middle = Math.floor(high / 2);
 	let iterations = 1;
 
-	while (values[middle] !== target && low < high) {
+	while (low <= high) {
+		const middle = Math.floor((low + high) / 2);
+		if (values[middle] === target) {
+			return {
+				iterations,
+				position: middle,
+			};
+		}
 		if (target < values[middle]) {
 			high = middle - 1;
 		} else {
 			low = middle + 1;
 		}
 
-		middle = Math.floor((high + low) / 2);
 		iterations++;
 	}
 
 	return {
 		iterations,
-		position: (values[middle] === target) ? middle : -1,
+		position: -1,
 	};
 }
 
-export default binarySearch;
+export function binarySearchRecursive<T>(
+	values: T[] = [],
+	target: T,
+	low: number = 0,
+	high: number = values.length - 1,
+	iterations: number = 1): IBinarySearchResult {
+
+	if (low <= high) {
+		const middle = Math.floor((low + high) / 2);
+		if (values[middle] === target) {
+			return {
+				iterations,
+				position: middle,
+			};
+		} else if (target < values[middle]) {
+			return binarySearchRecursive(values, target, low, middle - 1, ++iterations);
+		} else {
+			return binarySearchRecursive(values, target, middle + 1, high, ++iterations);
+		}
+	}
+
+	return {
+		iterations,
+		position: -1,
+	};
+}
